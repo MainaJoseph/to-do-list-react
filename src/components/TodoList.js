a8dc96dbde0f8073a8748c98fdd7cbb4c20e2c62
MainaJoseph <mugukumainajos@gmail.com>
2022-11-03 13:11:14 +0300

    second commit

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import TodoForm from './TodoForm';
 import Todo from './Todo';
+import '../App.css';
+import { useEffect } from "react";
 
 function TodoList() {
   const [todos, setTodos] = useState([]);
@@ -40,9 +42,52 @@ function TodoList() {
     setTodos(updatedTodos);
   };
 
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const toRotate = [ "Create Your Tasks now", "Easy", "Automated" ];
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => { clearInterval(ticker) };
+  }, [text])
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta(prevDelta => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex(prevIndex => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex(prevIndex => prevIndex + 1);
+    }
+  }
+  
+
   return (
     <>
       <h1>What's the Plan for Today?</h1>
+      <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Create Your Tasks now", "Easy", "utomated" ]'><span className="wrap">{text}</span></span>
       <TodoForm onSubmit={addTodo} />
       <Todo
         todos={todos}
